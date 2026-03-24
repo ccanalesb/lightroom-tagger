b1b00770d2620aa1c00728a081ce4e4ecf55f595
Cristian Canales <cristian@example.com>
2026-03-24 12:03:39 -0400

    chore: remove redundant DumpMediaPage

--- apps/visualizer/frontend/src/App.tsx
+++ apps/visualizer/frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Layout } from './components/Layout'
 import { DashboardPage } from './pages/DashboardPage'
 import { InstagramPage } from './pages/InstagramPage'
 import { MatchingPage } from './pages/MatchingPage'
-import { DumpMediaPage } from './pages/DumpMediaPage'
 import { JobsPage } from './pages/JobsPage'
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
           <Route index element={<DashboardPage />} />
           <Route path="instagram" element={<InstagramPage />} />
           <Route path="matching" element={<MatchingPage />} />
-          <Route path="dump-media" element={<DumpMediaPage />} />
           <Route path="jobs" element={<JobsPage />} />
         </Route>
       </Routes>
